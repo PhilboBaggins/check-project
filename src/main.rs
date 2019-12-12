1305db71b3cc43e4dc19f193dd4492d669e76149
Phil <PhilboBaggins@users.noreply.github.com>
2019-12-13 01:26:49 +1100

    Check for some of the fields in Cargo.toml files

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,54 @@ mod tests {
     }
 }
 
-fn check_cargo_project(proj_path: &str, verbose: u64) {
+fn check_field_string(field: Option<&String>, friendly_name: &str) {
+    if let Some(field) = field {
+        if field.contains("?") {
+            println!("{}: {}", friendly_name, field);
+        }
+    } else {
+        println!("{}: Not present", friendly_name);
+    }
+}
+
+fn compare_fields_string(field_1: Option<&String>, field_2: Option<&String>,
+    friendly_name_1: &str, friendly_name_2: &str) {
+    let not_present = "Not present".to_owned();
+    let field_1 = field_1.unwrap_or(&not_present);
+    let field_2 = field_2.unwrap_or(&not_present);
+    if field_1 != field_2 {
+        println!("{} and {} do not match", friendly_name_1, friendly_name_2);
+        println!("    {}: {}", friendly_name_1, field_1);
+        println!("    {}: {}", friendly_name_2, field_2);
+    }
+}
+
+fn check_cargo_project(proj_path: &str, _verbose: u64) {
     let cargo_toml_path = Path::new(proj_path).join("Cargo.toml");
     let cargo_toml_data = Manifest::<toml::Value>::from_slice_with_metadata(&read(cargo_toml_path).unwrap()).unwrap();
     let cargo_toml_package = cargo_toml_data.package.as_ref().unwrap();
 
+    // Check Cargo.toml fields
+    // TODO: &cargo_toml_package.edition
+    check_field_string(Some(&cargo_toml_package.version), "Cargo.toml version field");
+    check_field_string(cargo_toml_package.description.as_ref(), "Cargo.toml description field");
+    check_field_string(cargo_toml_package.repository.as_ref(), "Cargo.toml repository field");
+    check_field_string(cargo_toml_package.license.as_ref(), "Cargo.toml license field");
+
+    // .....................
     let git_config_path = Path::new(proj_path).join(".git").join("config");
     if let Ok(conf) = Ini::load_from_file(git_config_path) {
-        let section = conf.section(Some("remote \"origin\"".to_owned())).unwrap();
-
-        if verbose >= 1 {
-            println!("URL: {}", section.get("url").unwrap());
+        if let Some(remote_origin_section) = conf.section(Some("remote \"origin\"".to_owned())) {
+            compare_fields_string(
+                cargo_toml_package.repository.as_ref(),
+                remote_origin_section.get("url"),
+                "Cargo.toml repository field",
+                ".git/config remote origin URL");
         }
+    }
 
-        if let Some(repo_path) = &cargo_toml_package.repository {
-            println!("repo_path: {}", repo_path);
-        }
+    if let Some(_license_info) = &cargo_toml_package.license {
+        // TODO: Check for license files that match cargo_toml_package.license
     }
 
     // TODO: Add check for CI badge:
